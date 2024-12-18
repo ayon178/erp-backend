35439b7ead6b8f4c9b5004bd65d4b82dba210020
AyonJD <ayonjodder177@gmail.com>
2024-12-18 16:13:59 +0600

    [Init]: Project initialized and database connection

---
+++ database/connection.py
@@ -0,0 +1,9 @@
+from pymongo import MongoClient
+
+# Replace with your MongoDB URI
+MONGO_URI = "mongodb+srv://ayonjd178:ayonjd178@cluster0.wqxk9a6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+
+client = MongoClient(MONGO_URI)
+db = client.practice_fastapi
+rawItem_collection = db.raw_items
