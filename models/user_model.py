9f130876dd140ed4ad08f783d28031b4ae0b9aea
AyonJD <ayonjodder177@gmail.com>
2025-02-07 18:10:44 +0600

    Refactor UserModel fields
    This commit refactors the fields in the UserModel class. It adds a new field called member_serial_number and reorders the existing fields for better readability and consistency. The changes include updating the field names, adding a new field for designation, and reordering the fields to match the desired order. The category field is now optional.

--- models/user_model.py
+++ models/user_model.py
@@ -2,11 +2,13 @@ from pydantic import BaseModel
 from typing import Optional
 
 class UserModel(BaseModel):
-    name: str
-    address: str
-    phone: str
-    role: str
-    password: str
-    email: str
-    department: str 
-    category: Optional[str] = None
+    member_serial_number: int 
+    name: str                 
+    designation: str          
+    department: str            
+    email: str                 
+    phone1: str                
+    phone2: Optional[str] = None  
+    address: str               
+    password: str             
+    category: Optional[str] = None 
